be0bd64efd46bae3cfe8341323b3f82cf32ab588
Jaivishwa-dev <jaivishwa.dev@gmail.com>
2024-12-28 06:43:25 +0530

    successfully runned

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+
+
+class Project(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    image = models.ImageField(upload_to="portfolio/")
+    github_link = models.URLField(blank=True,null=True)
+
+    def __str__(self):
+        return self.title
+
+class Skill(models.Model):
+    name = models.CharField(max_length=50)
+    proficiency = models.IntegerField(help_text='Enter percentage (0-100')
+
+    def __str__(self):
+        return self.name
+class Testimonial(models.Model):
+    name = models.CharField(max_length=100)
+    desigination = models.CharField(max_length=100)
+    image = models.ImageField(upload_to="testimonials/")
+
+    def __str__(self):
+        return self.name
--- portfolio/views.py
+++ portfolio/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import  *
+
+def home(request):
+    projects = Project.objects.all()
+    skills = Skill.objects.all()
+    testimonials = Testimonial.objects.all()
+
+    return render(request,'portfolio/index.html',{
+        'projects' : projects,
+        'skills' : skills,
+        'testimonials' : testimonials
+    })
